1ef623ae54bd668d985b0d0ef964efdf3ee3c84f
Viper910 <beheraamitkumar156@gmail.com>
2023-02-15 18:06:56 +0530

    data modified

--- data.js
+++ data.js
@@ -3,15 +3,15 @@ const data = {
     carouselimages:[
         {
             _id:'1',
-            link:'../Images/Carouselimage1.jpg'
+            link:'/Images/Carouselimage1.jpg'
         },
         {
             _id:'2',
-            link:'../Images/Carouselimage2.jpg'
+            link:'/Images/Carouselimage2.jpg'
         },
         {
             _id:'3',
-            link:'../Images/Carouselimage3.jpg'
+            link:'/Images/Carouselimage3.jpg'
         },
 
     ]
